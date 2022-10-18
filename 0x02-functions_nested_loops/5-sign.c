58a12c0a389f064a8a9402b15ed04c84332e5735
ygbadamosi662 <gbadamosiy662@gmail.com>
2022-10-18 15:20:44 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: is the checked number
+ * Return: 1 if > 0,0 if == 0 and -1 if < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+	else if (n == 0)
+		_putchar(0);
+		return (0);
+	else if (n < 0)
+		_putchar('-');
+		return (0);
+}
