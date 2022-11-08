8f053aa38449c939e96be55c7b8dd4d67fe79bf8
ygbadamosi662 <gbadamosiy662@gmail.com>
2022-11-08 11:17:49 -0800

    corrected

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,9 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
+	array = (char *)malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		array[i] = c;
